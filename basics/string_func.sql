ab733fc9c98f3c0f9e6dc30ac235e3c14d88f327
chgwyellow <dinnis1107@gmail.com>
2026-01-07 23:01:14 +0800

    Combine string method.

--- basics/string_func.sql
+++ basics/string_func.sql
@@ -13,3 +13,17 @@ SELECT CONCAT_WS('-', title, author_fname, author_lname) FROM books;
 -- SUBSTRING
 SELECT SUBSTRING('Hello World', 1, 4); -- Hell
 
+-- Combine string methods
+SELECT CONCAT(
+        SUBSTR(title, 1, 10), 
+        '...'
+    ) AS 'short title' 
+FROM books;
+
+SELECT CONCAT(
+		SUBSTR(author_fname, 1, 1),
+		'.',
+		SUBSTR(author_lname, 1, 1),
+        '.'
+	) AS author_initial
+FROM books;
